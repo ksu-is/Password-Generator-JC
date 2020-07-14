6078a626e83b42859e1bc8c929b1f0f0efb48b90
jabari682 <65993218+jabari682@users.noreply.github.com>
2020-07-14 14:32:55 -0400

    Update code.py
    We add a yes or no option to the user interface. If the user do not like the password, the code will give the user a new password.

--- code.py
+++ code.py
@@ -4,3 +4,12 @@ password_length = int(input("enter length of required password"))
 password = "".join(random.sample(characters,password_length))
 print(password.upper())
 print(password.lower())
+answer = input("Do you like the password? Enter yes or no: ")
+if answer == "yes":
+    print("Thank You!")
+elif answer == "no":
+    password = "".join(random.sample(characters,password_length))
+    print(password.upper())
+    print(password.lower())
+else:
+    print("Please enter yes or no.")
