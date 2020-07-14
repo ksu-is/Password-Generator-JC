61c46a88d04541cc2465d1cdcfd4a1e514abe978
jabari682 <65993218+jabari682@users.noreply.github.com>
2020-07-14 14:14:39 -0400

    Update code.py
    We gave the user two new options one uppercase and one lowercase.

--- code.py
+++ code.py
@@ -2,4 +2,5 @@ import random
 characters = "abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%^&*()_-+[]{};/';.,"
 password_length = int(input("enter length of required password"))
 password = "".join(random.sample(characters,password_length))
-print(password)
+print(password.upper())
+print(password.lower())
